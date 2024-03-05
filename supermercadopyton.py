e2961a2e560c3c26099f5bfd7609db8dcf11794f
CarlosJulioGordilloCovaleda <160853185+CarlosJulioGordilloCovaleda@users.noreply.github.com>
2024-03-05 18:23:54 -0500

    Arreglo

--- supermercadopyton.py
+++ supermercadopyton.py
@@ -2,20 +2,21 @@ n=int(input("ingrese n : "))
 cantidad=int(input("Ingrese la cantidad de productos : "))
 precios=[]
 precio_Total=0
+descuento_inicial=0.2
+descuento_total=0
 for i in range(cantidad):
     precio=float(input(f"ingrese el precio del producto {i+1} "))
     precios.append(precio)
     precio_Total+=precio
-descuento_inicial=0.2
-descuento_total=0
-for i in precios:
-    if i<=n:
-        descuento_aplicado=descuento_inicial
-    else :
-        descuento_aplicado=(descuento_inicial)/(2**i//n)
-    descuento_producto=descuento_aplicado*i
-    descuento_total+=descuento_producto
-precio_final=precio_Total-descuento_total
-print(precio_Total)
-print(descuento_total)
-print(precio_final)
+    grupos_completos = (i - 1) // n
+    for i,precio in enumerate(precios,1):
+        if i <= n:
+            descuento_aplicado=descuento_inicial
+        else:
+            
+            descuento_aplicado = descuento_inicial / (2 ** grupos_completos)
+
+    descuento_producto = descuento_aplicado * precio
+    descuento_total += descuento_producto
+print("Precio total:", precio_Total)
+print("Descuento total:", descuento_total)
